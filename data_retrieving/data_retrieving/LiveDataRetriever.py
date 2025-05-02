1daaeb5e554d0641e6938402c6970ad1f3e177da
noahpy <noschl@proton.me>
2025-05-02 17:48:25 +0200

    improve LiveDataRetriever Rate limit check to continuous check

--- data_retrieving/data_retrieving/LiveDataRetriever.py
+++ data_retrieving/data_retrieving/LiveDataRetriever.py
@@ -2,149 +2,120 @@
 from pybit.unified_trading import HTTP as BybitSession
 import json
 import time
-from threading import Thread
-from multiprocessing import Process, Lock, Value
+from multiprocessing import Process, Queue, Value
 import signal
 
-request_mutex = Lock()
-request_count = Value('i', 0)
+RATELIMIT_PER_FIVE_SECONDS = 600
+request_reset_process_started = Value('b', False)
+# intialize a queue with max size of ratelimit
+request_queue = Queue(RATELIMIT_PER_FIVE_SECONDS - 1)
 
 WAIT_TIME_LIMIT = 0.1
 
+REPORT_REFRESH_RATE = 3
+
 class LiveDataRetriever:
-    """Singleton class for fetching live data.
-       There can only exist one instace of this class per process
-       (See https://code.activestate.com/recipes/52558/).
-       Across multiple processes, the mutex and request count will be shared,
-       ensuring that the rate limit is not exceeded.
-       After usage, this class should be deleted using del ld, to ensure a
-       graceful exit.
+    """Class for fetching live data.
+       Has respective methods to fetch current orderbook and recent trading history data.
+       Implements rate limiting.
 
     @params:
         key_file_path: path to JSON key file
 
     """
 
-    class LiveDataRetrieverImpl:
-
-        def __init__(self, key_file_path: str):
-            self.session = self.create_session(key_file_path)
-            self.RATELIMIT_PER_FIVE_SECONDS = 600
-            self.loop_refresh = True
-            self.refresh_thread = Thread(
-                target=self.periodic_reset_request_count)
-            self.refresh_thread.start()
-
-        def __del__(self):
-            self.loop_refresh = False
-            time.sleep(0.1)
-
-        def periodic_reset_request_count(self):
-            last_time = time.time()
-            while self.loop_refresh:
-                time.sleep(0.02)
-                if (time.time() - last_time >= 4.9):
-                    last_time = time.time()
-                    with request_mutex:
-                        print("Reset rate limit counter from: ",
-                              request_count.value)
-                        request_count.value = 0
-
-        def create_session(self, key_file_path: str) -> BybitSession:
-            """
-            Create a BybitSession object from a JSON key file.
-            """
-            with open(key_file_path) as f:
-                keys = json.load(f)
-            session = BybitSession(
-                api_key=keys['key'], api_secret=keys['secret'])
-            return session
-
-        def fetch_current_orderbook(self, symbol: str,
-                                    category: str = "spot", limit: int = 200) -> dict:
-            """
-            Fetch current orderbook data.
-            category can be: spot, inverse, linear
-            """
-            if (limit < 0 or limit > 200):
-                limit = 200
-            try:
-                loop = True
-                start_time = time.time()
-                while loop:
-                    with request_mutex:
-                        if (request_count.value < self.RATELIMIT_PER_FIVE_SECONDS):
-                            request_count.value += 1
-                            loop = False
-                            break
-                    time.sleep(0.01)
-                wait_time = time.time() - start_time
-                if wait_time >= WAIT_TIME_LIMIT:
-                    raise Exception("Wait time too long: " + str(wait_time))
-
-                orderbook = self.session.get_orderbook(
-                    category=category, symbol=symbol, limit=limit)
-                return orderbook["result"]
-            except Exception as e:
-                print("Error fetching orderbook!")
-                print(e)
-
-        def fetch_recent_trading_history(self, symbol: str, category: str = "spot", limit: int = 500):
-            """
-            Fetch recent trading history.
-            category can be: spot, inverse, linear, option
-            """
-
-            if (category == "spot"):
-                if (limit < 0 or limit > 60):
-                    limit = 60
-            else:
-                if (limit < 0 or limit > 1000):
-                    limit = 1000
-            try:
-                loop = True
-                start_time = time.time()
-                while loop:
-                    with request_mutex:
-                        if (request_count.value < self.RATELIMIT_PER_FIVE_SECONDS):
-                            request_count.value += 1
-                            loop = False
-                            break
-                    time.sleep(0.01)
-                wait_time = time.time() - start_time
-                if wait_time >= WAIT_TIME_LIMIT:
-                    raise Exception("Wait time too long: " + str(wait_time))
-                trades = self.session.get_public_trade_history(
-                    category=category, symbol=symbol, limit=limit)
-                return trades["result"]
-            except Exception as e:
-                print("Error fetching recent trades!")
-                print(e)
-
-    __instance = None
-
     def __init__(self, key_file_path: str):
-        """ Create singleton instance """
-        # Check whether we already have an instance
-        if LiveDataRetriever.__instance is None:
-            # Create and remember instance
-            LiveDataRetriever.__instance = LiveDataRetriever.LiveDataRetrieverImpl(
-                key_file_path)
-
-        # Store instance reference as the only member in the handle
-        self.__dict__[
-            '_LiveDataRetriever__instance'] = LiveDataRetriever.__instance
-
-    def __getattr__(self, attr):
-        """ Delegate access to implementation """
-        return getattr(self.__instance, attr)
-
-    def __setattr__(self, attr, value):
-        """ Delegate access to implementation """
-        return setattr(self.__instance, attr, value)
+        self.session = self.create_session(key_file_path)
+        self.loop_refresh = True
+        self.refresh_process = None
+        if not request_reset_process_started.value:
+            self.refresh_process = Process(
+                target=self.ratelimit_queue_clearance)
+            self.refresh_process.start()
+            request_reset_process_started.value = True
 
     def __del__(self):
-        self.__instance.__del__()
+        self.loop_refresh = False
+        if self.refresh_process:
+            self.refresh_process.terminate()
+        time.sleep(0.1)
+
+    def ratelimit_queue_clearance(self):
+        def handle_sigint(signum, frame):
+            print("Received SIGINT at queue process, cleaning up...")
+            exit(0)
+
+        signal.signal(signal.SIGINT, handle_sigint)
+        report_time = time.time()
+        while self.loop_refresh:
+            if time.time() - report_time >= REPORT_REFRESH_RATE:
+                print(f"Made {request_queue.qsize()} request in past {REPORT_REFRESH_RATE} seconds.")
+                report_time = time.time()
+            time.sleep(0.01)
+            # requests before 5 seconds can be dismissed
+            dismiss_threshhold = time.time() - 5
+            req = request_queue.get()
+            while req <= dismiss_threshhold:
+                dismiss_threshhold = time.time() - 5
+                req = request_queue.get()
+            time.sleep(req - dismiss_threshhold)
+
+    def create_session(self, key_file_path: str) -> BybitSession:
+        """
+        Create a BybitSession object from a JSON key file.
+        """
+        with open(key_file_path) as f:
+            keys = json.load(f)
+        session = BybitSession(
+            api_key=keys['key'], api_secret=keys['secret'])
+        return session
+
+    def fetch_current_orderbook(self, symbol: str,
+                                category: str = "spot", limit: int = 200) -> dict:
+        """
+        Fetch current orderbook data.
+        category can be: spot, inverse, linear
+        """
+        if (limit < 0 or limit > 200):
+            limit = 200
+        try:
+            start_time = time.time()
+            request_queue.put(time.time())
+            wait_time = time.time() - start_time
+            if wait_time >= WAIT_TIME_LIMIT:
+                print("Wait time too long: " + str(wait_time))
+
+            orderbook = self.session.get_orderbook(
+                category=category, symbol=symbol, limit=limit)
+            return orderbook["result"]
+        except Exception as e:
+            print("Error fetching orderbook!")
+            print(e)
+
+    def fetch_recent_trading_history(self, symbol: str, category: str = "spot", limit: int = 500):
+        """
+        Fetch recent trading history.
+        category can be: spot, inverse, linear, option
+        """
+
+        if (category == "spot"):
+            if (limit < 0 or limit > 60):
+                limit = 60
+        else:
+            if (limit < 0 or limit > 1000):
+                limit = 1000
+        try:
+            start_time = time.time()
+            request_queue.put(time.time())
+            wait_time = time.time() - start_time
+            if wait_time >= WAIT_TIME_LIMIT:
+                print("Wait time too long: " + str(wait_time))
+            trades = self.session.get_public_trade_history(
+                category=category, symbol=symbol, limit=limit)
+            return trades["result"]
+        except Exception as e:
+            print("Error fetching recent trades!")
+            print(e)
 
 
 if __name__ == "__main__":
@@ -157,13 +128,13 @@ if __name__ == "__main__":
 
     def loop():
         signal.signal(signal.SIGINT, interpret_sigint)
-        ld = LiveDataRetriever("apiKey.json")
+        ld = LiveDataRetriever("api_key.json")
         while (True):
             ld.fetch_current_orderbook("BTCUSDT")
 
     def loop2():
         signal.signal(signal.SIGINT, interpret_sigint)
-        ld = LiveDataRetriever("apiKey.json")
+        ld = LiveDataRetriever("api_key.json")
         while (True):
             ld.fetch_recent_trading_history("BTCUSDT")
 
